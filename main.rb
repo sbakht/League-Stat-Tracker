8a07ebb1de0d4c2c2f2bd6b2e25c112434096ee4
Saad Bakht <rotk13@gmail.com>
2013-11-01 20:17:49 -0400

    Search match history by champ

--- main.rb
+++ main.rb
@@ -7,12 +7,19 @@ DB = Sequel.connect('sqlite://league.db')
 
 
 get '/' do
-	updateStats
+	#updateStats
 	posts = DB["24174733".intern]
 	@post = posts.all
 	erb :main
 end
 
+get '/:playerid' do
+	@playerid = params[:playerid]
+	@playerStats = DB[params[:playerid].intern]
+	#@playerStats = @playerStats.select(:outcome).where(:champion => 'Twitch').all
+	#@playerStats = @playerStats.all
+	erb :playerpage
+end
 
 
 helpers do  
@@ -25,4 +32,9 @@ Thread.new do # trivial example work thread
   	sleep 3600
     updateStats
   end
+end
+
+post '/plswork' do
+	require 'json'
+	return DB[params[:playerid].intern].select(:outcome).where(:champion => params[:herp]).all.to_json
 end
---
+++ test.rb
@@ -0,0 +1,3 @@
+require 'json'
+car = [{:make => "wow", :year => "2004"}, {:make => "bmw", :year => "2003"}]
+puts car.to_json
